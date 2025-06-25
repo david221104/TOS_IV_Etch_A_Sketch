142f263fe0b5549b4d17cc5cab21c157122c34a3
david221104 <davidkrdzo.2@gmail.com>
2025-06-25 20:34:58 +0200

    add hovering

--- work.js
+++ work.js
@@ -3,7 +3,10 @@ const container = document.querySelector(".container");
 function canvas() {
     for(let i = 0; i < 16; i++) {
         for(let y = 0; y < 16; y++) {
-            const grid = document.createElement("div");    
+            const grid = document.createElement("div");
+            grid.addEventListener("mouseover", () => {
+                grid.setAttribute("style", "background-color: black")
+            });    
             container.appendChild(grid);
         }
     }
