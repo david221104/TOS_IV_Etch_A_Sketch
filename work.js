eda456f6498fc9d83749c77d30a57193962c0b67
david221104 <davidkrdzo.2@gmail.com>
2025-06-26 14:22:10 +0200

    add input box, set prototype number of squares

--- work.js
+++ work.js
@@ -1,9 +1,30 @@
 const container = document.querySelector(".container");
 
-function canvas() {
-    for(let i = 0; i < 16; i++) {
-        for(let y = 0; y < 16; y++) {
+const input = document.querySelector("#gridSize");
+function inputButtonFunction() {
+    let values = document.querySelector("#gridSize").value;
+    console.log(values);
+    input.value="";
+    if(values >=1 && values <= 100){
+        return values;
+    }
+    else{
+        alert("Wrong input, try again");
+    }
+}
+
+const inputButton = document.querySelector("#inputButton");
+inputButton.addEventListener("click", inputButtonFunction);
+
+let inputSizeGrid = 100;
+
+//function that prints the grid
+function canvas(inputNumber) {
+    for(let i = 0; i < inputNumber ; i++) {
+        for(let y = 0; y < inputNumber; y++) {
             const grid = document.createElement("div");
+            let inputSize = 640 / inputNumber;
+            grid.setAttribute("style", `width: ${inputSize}px; height: ${inputSize}px`);
             grid.addEventListener("mouseover", () => {
                 grid.setAttribute("style", "background-color: black")
             });    
@@ -12,4 +33,5 @@ function canvas() {
     }
 }
 
-canvas();
+canvas(inputSizeGrid);
+
